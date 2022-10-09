ffcd93f8dc23d0a3273bf1d0684f31f89f28ed0d
Carlosgs21 <113357372+Carlosgs2001@users.noreply.github.com>
2022-10-09 19:46:38 +0000

    added horse

--- Horse.java
+++ Horse.java
@@ -1,1 +1,21 @@
 //Create the Horse class here.
+public class Horse {
+String name;
+double weight;
+boolean tame;
+
+  public Horse(String name, double weight, boolean tame){
+    this.name = name;
+    this.weight = weight;
+    this.tame = tame;
+  }
+  public void addName(String nameh){
+    this.name = nameh;
+  }
+  public void addWeight(double weighth){
+    this.weight = weighth;
+  }
+  public void addTame(boolean tameh){
+    this.tame = tameh;
+  }
+}
